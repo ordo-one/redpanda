f1bb2427b150a0700def0b7bf3568d3117c7351b
Travis Downs <travis.downs@vectorized.io>
2024-05-02 15:14:25 -0400

    DELETEME: fix advertised connections to 360000
    Same as T6.

--- tests/rptest/redpanda_cloud_tests/omb_validation_test.py
+++ tests/rptest/redpanda_cloud_tests/omb_validation_test.py
@@ -205,8 +205,7 @@ class OMBValidationTest(RedpandaCloudTest):
         # fill with swarm connections: we add 10% to the nominal amount to ensure we test the
         # advertised limit and this uses up ~half the slack we have in the enforcement (we currently
         # set the per broker limit to 1.2x of what it would be if enforced exactly).
-        swarm_target_connections = int(
-            (tier_limits.max_connection_count * 6 - omb_connections) * 1.1)
+        swarm_target_connections = int((360000 - omb_connections) * 1.1)
 
         # we expect each swarm producer to create 1 connection per broker, plus 1 additional connection
         # for metadata
